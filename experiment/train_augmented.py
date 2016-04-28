b15700c0d2ae86a27a94744f6863b29fb3d3bf61
qianguih <qianguih@usc.edu>
2016-04-27 23:43:25 -0700

    update train_augmented.py
    update train_augmented.py to deal with dynamic parameter setting

--- experiment/train_augmented.py
+++ experiment/train_augmented.py
@@ -19,14 +19,6 @@ from neon.layers import RecurrentSum, RecurrentMean
 from networks import load_core, get_title_augmentor
 from data import get_imdb, get_saudinet_data
 
-# parse the command line arguments
-parser = NeonArgparser(__doc__)
-args = parser.parse_args(gen_be=False)
-
-# hyperparameters from the reference
-args.batch_size = 32
-args.backend = 'gpu'
-
 
 class MultiModalModel(Model):
 
@@ -51,17 +43,66 @@ class MultiModalModel(Model):
 
 
 if __name__ == '__main__':
+    # parse the command line arguments
+    parser = NeonArgparser(__doc__)
+    parser.add_argument("new_data_source", help = "choose the data source used for classification. options: content, title, author, author-content, author-title, content-title")
+    parser.add_argument("initial_model", help = "choose the initial encoder-decoder model. options: content, title, author")
+    
+
+    args = parser.parse_args(gen_be=False)
+
+    # hyperparameters from the reference
+    args.batch_size = 32
+    args.backend = 'gpu'
+    
+    print ' '
+    print "new source used for classification is: ", args.new_data_source
+    print "initial model is trained on: ", args.initial_model
+    print ' '
+    
+    
     # Setup backend
     be = gen_backend(**extract_valid_args(args, gen_backend))
 
     # Load the dataset
     (X_train_content, y_train_content), (X_test_content, y_test_content), nout, vocab_size = get_saudinet_data(args, modality='content')
-    (X_train_title, y_train_title), (X_test_title, y_test_title), nout, vocab_size = get_saudinet_data(args, modality='authors')
-    train_set = ArrayIterator([X_train_content, X_train_title], y_train_content, nout)
-    test_set = ArrayIterator([X_test_content, X_test_title], y_test_content, nout)
+    (X_train_title, y_train_title), (X_test_title, y_test_title), nout, vocab_size = get_saudinet_data(args, modality='title')
+    (X_train_authors, y_train_authors), (X_test_authors, y_test_authors), nout, vocab_size = get_saudinet_data(args, modality='author')
+    
+    
+    X_train, X_test = {}, {}
+    X_train['content-author'], X_test['content-author'] = [X_train_content,X_train_authors], [X_test_content,X_test_authors]
+    X_train['content-title'], X_test['content-title'] = [X_train_content,X_train_title], [X_test_content,X_test_title]
+    
+    X_train['title-author'], X_test['title-author'] = [X_train_title,X_train_authors], [X_test_title, X_test_authors]
+    X_train['title-content'], X_test['title-content'] = [X_train_title,X_train_content], [X_test_title, X_test_content]
+    
+    X_train['author-content'], X_test['author-content'] = [X_train_authors, X_train_content], [X_test_authors, X_test_content]
+    X_train['author-title'], X_test['author-title'] = [X_train_authors, X_train_title], [X_test_authors, X_test_title]
+
+    X_train['content-author-title'], X_test['content-author-title'] = [X_train_content,X_train_authors,X_train_title], [X_test_content,X_test_authors,X_test_title]
+
+    X_train['title-author-content'], X_test['title-author-content'] = [X_train_title,X_train_authors,X_train_content], [X_test_title, X_test_authors,X_test_content]
+
+    X_train['author-content-title'], X_test['author-content-title'] = [X_train_authors, X_train_content,X_train_title], [X_test_authors, X_test_content,X_test_title]
+
+
+    
+    
+    data_source_keyword = args.initial_model + '-' + args.new_data_source
+    
+    
+    train_set = ArrayIterator( X_train[data_source_keyword], y_train_content, nout)
+    test_set = ArrayIterator(  X_test[data_source_keyword],  y_test_content,  nout)
+    
+    
+    
+    #train_set = ArrayIterator([X_train_content, X_train_title], y_train_content, nout)
+    #test_set = ArrayIterator([X_test_content, X_test_title], y_test_content, nout)
 
     # Build the network
-    encoder, decoder = load_core()
+    file_prefix = './saved_models/' + args.initial_model
+    encoder, decoder = load_core(file_prefix)
     encoder = Sequential(encoder)
     augmentor, cost, opt = get_title_augmentor(vocab_size=vocab_size)
     augmentor = Sequential(augmentor)
@@ -85,5 +126,3 @@ if __name__ == '__main__':
     print 'Train accuracy: ', model.eval(train_set, metric=Accuracy())
     print 'Test accuracy: ', model.eval(test_set, metric=Accuracy())
 
-    # Save model
-    # TODO: Export augmentor
